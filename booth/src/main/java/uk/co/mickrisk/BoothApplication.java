757b1b103f3654d32823fed4a4d17223c636ffa4
mike <mike@mickrisk.com>
2016-06-13 23:46:50 +0100

    Added townhall

--- booth/src/main/java/uk/co/mickrisk/BoothApplication.java
+++ booth/src/main/java/uk/co/mickrisk/BoothApplication.java
@@ -7,6 +7,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.google.common.base.Predicate;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -29,10 +31,14 @@ public class BoothApplication {
         return new Docket(DocumentationType.SWAGGER_2)  
           .select()                                  
           .apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.ant("/vote/*"))                          
+          .paths(getPaths())                          
           .build()
           .apiInfo(apiInfo());                                           
     }
+
+	private Predicate<String> getPaths() {
+		return PathSelectors.any();
+	}
 
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder().title("Voting Booth")
